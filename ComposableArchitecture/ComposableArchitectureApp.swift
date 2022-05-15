71c329af388b1a1e056a436e0993882102392734
Jamesf807 <jamesfisher807@gmail.com>
2022-05-14 18:36:14 -0800

    Initial Commit

---
+++ ComposableArchitecture/ComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureApp.swift
+//  ComposableArchitecture
+//
+//  Created by James Fisher on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
